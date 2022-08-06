fbf67fc80cc6e4bc7804c4b384748a897768c8c9
Dipanshu Chhanikar <dipanshuchhanikar@gmail.com>
2022-08-06 18:07:50 +0530

    basic qr generator

---
+++ qr_code.py
@@ -0,0 +1,6 @@
+import qrcode as qr                                         #importing qrcode library
+
+img = qr.make("https://github.com/Dipanshu-Chhanikar")      #specifying the url for qr code
+
+img.save("github_profile.png")                              #saving the qr code in image format
+
